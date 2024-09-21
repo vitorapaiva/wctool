b6148f3e3c6396cbbb9201bb4d1fac6f53f5c6b0
Vitor Paiva <vitorpaiva@MacBook-Pro-de-Vitor.local>
2024-09-21 00:32:27 -0300

    Adding functions

--- src/wctool.clj
+++ src/wctool.clj
@@ -1,11 +1,35 @@
-(ns wctool)
+(ns wctool
+  (:require [clojure.java.io :as io]
+            [clojure.string :refer [split]]))
 
-(defn input-size
-  "Returns the size of the string it receives."
-  [line]
-  (if (= line "1234")
-    (println "Correct")
-    (println "Wrong")))
+(defn c-command [value]
+  (print (.length (io/file value))))
 
-(defn run [opts]
-  (println (input-size (read-line))))
+(defn l-command [value]
+  (print (count (line-seq (io/reader value)))))
+
+(defn w-command [value]
+  (-> (slurp value)
+      (split #"\W+")
+      (count)
+      (print)))
+
+(defn m-command [value]
+  (-> (slurp value)
+      (count)
+      (print)))
+
+(defn default-command [value]
+  (print (c-command value) (l-command value) (w-command value)))
+
+(defn run [& args]
+  (doseq [arg args]
+    (let [command (str (first (first arg)))
+          value (str (second (first arg)))]
+      (case command 
+        "-c" (c-command value)
+        "-l" (l-command value)
+        "-w" (w-command value)
+        "-m" (m-command value)
+        (default-command value))
+      (print " " value))))
